1f78437027cd194700adac50f6c33f3c78459b35
许姣 <xujiao@ftsland.com>
2018-02-07 23:15:15 +0800

    修复目录

--- crypto/bcrypt/bcrypt.go
+++ crypto/bcrypt/bcrypt.go
@@ -4,7 +4,7 @@
 
 // Package bcrypt implements Provos and Mazières's bcrypt adaptive hashing
 // algorithm. See http://www.usenix.org/event/usenix99/provos/provos.pdf
-package bcrypt // import "golang.org/x/crypto/bcrypt"
+package bcrypt // import "github.com/gionna/x/crypto/bcrypt"
 
 // The code is a port of Provos and Mazières's C implementation.
 import (
@@ -14,7 +14,6 @@ import (
 	"fmt"
 	"io"
 	"strconv"
-
 	"github.com/gionna/x/crypto/blowfish"
 )
 
